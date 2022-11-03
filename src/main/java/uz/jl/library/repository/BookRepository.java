063492992521b7a0f242d4348d6d33126f3fa3f1
Vaxobov-Siroj <vaxobovsiroj1998@gmail.com>
2022-11-03 23:24:19 +0500

    go

--- src/main/java/uz/jl/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package uz.jl.library.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.jl.library.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
